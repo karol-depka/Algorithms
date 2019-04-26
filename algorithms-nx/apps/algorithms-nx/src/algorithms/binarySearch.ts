3c7847a65908c9c4cd12a7134655eb4bfecaf018
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2019-04-26 06:31:42 +0200

    Fix `while ( start <= end )`

--- algorithms-nx/apps/algorithms-nx/src/algorithms/binarySearch.ts
+++ algorithms-nx/apps/algorithms-nx/src/algorithms/binarySearch.ts
@@ -5,8 +5,9 @@ export function binarySearch<T>(array: Array<T>, searchVal: T): number | undefin
   do {
     // let pivot = Math.abs(Math.round((start + end) / 2))
     let pivot = Math.round((start + end) / 2)
-    console.log('start', start, 'end: ', end, 'pivot: ', pivot, array, searchVal)
+    console.log('start', start, 'end: ', end, 'pivot: ', pivot, 'array: ', array, 'searchVal: ', searchVal)
     let valAtPivot = array[pivot]
+    console.log('valAtPivot', valAtPivot)
     // 0 1 0 ......
     if ( valAtPivot === searchVal ) {
       ret = pivot
@@ -20,7 +21,7 @@ export function binarySearch<T>(array: Array<T>, searchVal: T): number | undefin
       // d -> [a b *c* >d]
       // pivot = Math.round(pivot + ( end - pivot ) / 2)
     }
-  } while ( start < end )
+  } while ( start <= end )
 
   return ret
 }
