e1ff1757fa653be16ff0a33711021cebd7e4d99a
Karol Depka Pradzinski <karol-depka@users.noreply.github.com>
2019-04-26 05:28:08 +0200

    Satisfied some binarySearch cases ;)

--- algorithms-nx/apps/algorithms-nx/src/algorithms/binarySearch.ts
+++ algorithms-nx/apps/algorithms-nx/src/algorithms/binarySearch.ts
@@ -4,8 +4,8 @@ export function binarySearch<T>(array: Array<T>, searchVal: T): number | undefin
   let ret: number | undefined
   do {
     // let pivot = Math.abs(Math.round((start + end) / 2))
-    let pivot = Math.floor((start + end) / 2)
-    console.log('s', start, end, pivot, array, searchVal)
+    let pivot = Math.round((start + end) / 2)
+    console.log('start', start, 'end: ', end, 'pivot: ', pivot, array, searchVal)
     let valAtPivot = array[pivot]
     // 0 1 0 ......
     if ( valAtPivot === searchVal ) {
@@ -14,13 +14,13 @@ export function binarySearch<T>(array: Array<T>, searchVal: T): number | undefin
     } if ( valAtPivot > searchVal) {
       // b -> [a >b *b2* c d]
       end = pivot - 1
-      pivot = Math.round(pivot / 2)
+      // pivot = Math.round(pivot / 2)
     } else { // valAtPivot < searchVal
       start = pivot + 1
       // d -> [a b *c* >d]
       // pivot = Math.round(pivot + ( end - pivot ) / 2)
     }
-  } while ( start !== end )
+  } while ( start < end )
 
   return ret
 }
